54ac24d533429cfa0d7a5f2e36b3bf27bb0188d6
Eva Mayer <eva.mayer@hyperisland.se>
2022-12-05 13:36:41 +0100

    solved day 4 part 1

---
+++ day 4/day_4a.py
@@ -0,0 +1,19 @@
+def strip(element):
+    return element.strip()
+
+file = open("day4_input.txt")
+
+stripped_list = list(map(strip, file.readlines()))
+
+counts = 0
+for element in stripped_list:
+    a, b = element.split(',')
+    c, d = a.split('-'), b.split('-')
+    c = list(map(int, c))
+    d = list(map(int, d))
+    rng1 = set(range(c[0], c[1] + 1))
+    rng2 = set(range(d[0], d[1] + 1))
+    if rng1.issubset(rng2) or rng2.issubset(rng1):
+        counts += 1
+
+print(counts)
